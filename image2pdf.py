a6aa0060fc212b673b2b9b9c60c27d886bd1a80e
SAKTHI-STARK <sakthi492003@gmail.com>
2024-11-03 20:18:54 +0530

    selecting multiple files to convert to pdf

--- image2pdf.py
+++ image2pdf.py
@@ -1,19 +1,21 @@
 import img2pdf as i2p
 import select_directory as sd
+from PIL import Image
 global file
+#function to get the image files which are converte into pdf
 def read_file():
     global file
     file=sd.select_file()
-    with open(file,'r'):
-        pass
-
+    print(file)
+#function to convert single or multiple images to pdf
 def convert_img_to_pdf():
     global file
-    print(file)
-    print(type(file))
-    with open(sd.save_file(),'wb') as f:
+    save_dir=sd.save_file()
+    with open(save_dir,'ab') as f:
         page_size = (595, 842)
         f.write(i2p.convert(file,page_size=page_size))
-        file.close()
-        f.close()
+             
+            
 
+read_file()
+convert_img_to_pdf()
