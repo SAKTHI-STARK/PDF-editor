a656218c4226a7e963a64695c86dfc8409b26b77
SAKTHI-STARK <sakthi492003@gmail.com>
2024-11-09 21:31:21 +0530

    create separate function

--- split_pdf.py
+++ split_pdf.py
@@ -9,6 +9,7 @@ def split_pdf(input_pdf, start_page, end_page):
         page = pdf_reader.pages[page_num]
         pdf_writer.add_page(page)
     #saving the newly created pdf file
-    with open(sd.save_file(), 'wb') as output_file:
-        pdf_writer.write(output_file)
-split_pdf(sd.select_file(), 1, 6)  
+    def savefile():
+        with open(sd.save_file(), 'wb') as output_file:
+            pdf_writer.write(output_file)
+
